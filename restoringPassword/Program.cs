1131d1a86681d9fb17585bc2a83204abb8bda5d7
unknown <islammohamedahmed49@gmail.com>
2024-04-14 14:20:29 +0200

    rp

---
+++ restoringPassword/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+using static System.Convert;
+using static System.Math;
+
+
+string mainStr, compStr,res="";
+string[] str = new string[11];
+int indicator = 0; 
+
+
+mainStr = ReadLine();
+
+
+for(int i = 0; i < 10; i++)
+{
+    str[i] = ReadLine();
+}
+
+for(int j = 0; j < 8; j++)
+{
+
+    compStr = mainStr.Substring(j * 10, 10);
+    for (int k = 0; k < 10; k++)
+    {
+        if (str[k] == compStr)
+        {
+            res += k;
+            break;
+        }
+    }
+    indicator += 10;
+}
+
+WriteLine(res);
